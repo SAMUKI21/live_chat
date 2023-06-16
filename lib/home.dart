5ed7b083b793654f1225f54afb7527cf4779f89c
SAMUKI21 <107455429+SAMUKI21@users.noreply.github.com>
2023-06-16 21:17:43 +0530

    colours add two divide parts

--- lib/home.dart
+++ lib/home.dart
@@ -1,11 +1,72 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class Homescreen extends StatelessWidget {
   const Homescreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: const Color(0xFFCDB8F9),
+      body: CustomPaint(
+        painter: MyShape(),
+        child: Container(),
+      ),
+    );
+  }
+}
+
+class MyShape extends CustomPainter {
+  @override
+  // void paint(Canvas canvas, Size size) {
+  //   // TODO: implement paint
+  // }
+  
+
+void paint(Canvas canvas, Size size) {
+  // TODO: implement paint
+  final paint = Paint();
+  final path = Path();
+  // paint.color = Colors.lightGreen;
+  // paint.style = PaintingStyle.stroke;
+  paint.strokeWidth = 5;
+  path.moveTo(0, size.height * 0.0);
+  path.quadraticBezierTo(
+    size.width * 0.1,
+    size.height * 0.0,
+    size.width * 0.3,
+    size.height * 0.0,
+  );
+  path.quadraticBezierTo(
+    size.width * 0.45,
+    size.height * 0.0,
+    size.width * 0.5,
+    size.height * 0.0,
+  );
+  // path.quadraticBezierTo(
+  //   size.width * 0.95,
+  //   size.height * 0.78,
+  //   size.width * 0.7,
+  //   size.height * 0.5,
+  // );
+  path.quadraticBezierTo(
+    size.width * 0.95,
+    size.height * 0.78,
+    size.width,
+    size.height * 0.9,
+  );
+  path.lineTo(size.width, size.height);
+  path.lineTo(0, size.height);
+  path.lineTo(0, size.height * 0.8);
+
+  canvas.drawPath(path, paint);
+  final paint1 = Paint();
+  paint1.color = const Color(0xFFBAA81F7);
+  paint1.style = PaintingStyle.fill;
+  canvas.drawPath(path, paint1);
+}
+@override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
   }
 }
